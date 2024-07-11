c9ac7dcb06ddc531a4e9c98941f35037a1de1453
PUSHKAR SINGH GURJAR <pushkargurjar1234@gmail.com>
2024-07-12 00:31:30 +0530

    commits on date 12

---
+++ 11_july/size.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char arr[50];
+    cin>>arr;
+    int count=0;
+    for(int i=0;arr[i]!='\0';i++){
+        count++;
+    }
+    cout<<count<<endl;
+    
+}
